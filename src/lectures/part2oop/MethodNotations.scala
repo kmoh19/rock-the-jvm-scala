b3a4a5e9c75c715e7404003afd5510bdc937e587
kmoh19 <kmoh19@gmail.com>
2020-04-14 12:36:02 +0000

    method notations updated

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -1,14 +1,20 @@
 package lectures.part2oop
-import scala.language.postfixOps
+import scala.language.postfixOps //explicit import as now deprecated
 
 object MethodNotations extends App{
-  class Person(val name: String, favoriteMovie: String){
+  class Person(val name: String, favoriteMovie: String, val age:Int=0){
     def likes(movie:String):Boolean =movie==favoriteMovie
     def hangOutWith(person:Person): String = s"${this.name} is hanging out with ${person.name}"
     def +(person:Person): String = s"${this.name} is hanging out with ${person.name}" //permissive method naming in scala
-    def unary_! :String=s"$name,WTF!"
+    def +(nickname:String): Person = new Person(s"$name ($nickname)", favoriteMovie)//operator overloading
+    def unary_! :String=s"$name,WTF!" //note space between unary function name and colon is very important
+    def unary_+ :Person = new Person(name, favoriteMovie,age+1)
     def isAlive: Boolean =true
     def apply(): String = s"Hi my name is $name and i like $favoriteMovie"
+    def apply(n:Int):String=s"$name watched $favoriteMovie $n times"
+
+    def learns(thing:String) = s"$name is learning $thing"
+    def learnsScala =this learns "Scala" //infix :)
   }
   val mary = new Person("mary","inception")
   println(mary.likes("inception"))
@@ -31,12 +37,17 @@ object MethodNotations extends App{
 
   println(!mary)//see definition of operator above
   println(mary.unary_!)
+  println((+mary).age)
 
   //we also have postfix notations too but there are not popular...can only be used with methods that do not take any arguments
   println(mary isAlive)//postfix notation...now deprecated
   println(mary.isAlive)// we just use the dot notation vs the post fix
+  println(mary learnsScala)
 
   //apply
   println(mary())//apply notation
   println(mary.apply())
+  println((mary + "the rockstar")())
+  println(mary(10))
+
 }
